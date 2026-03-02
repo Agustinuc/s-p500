4d185ef9490f6a0c5311948b4874061f6b11c049
agustin hernandez Margozzini <89665265+Agustinuc@users.noreply.github.com>
2026-03-02 12:30:43 -0300

    Update syp.py

--- syp.py
+++ syp.py
@@ -13,8 +13,8 @@ SYMBOL = "^GSPC"
 LEVELS = [5, 10, 15, 20]  # niveles de alerta en %
 STATE_FILE = "sp500_state.json"
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_BOT_TOKEN = "8756159949:AAE-Nd2pI0mASrFH-6kbOSW_kRVGPtW7sJU"
+TELEGRAM_CHAT_ID = "-5178095003"
 
 
 # ==============================
